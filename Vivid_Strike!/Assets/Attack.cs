0e15ffc4502c7751b3f66107e546d61a9691aaa9
EBSiNoah <kyb0809tg@naver.com>
2019-08-21 22:18:32 +0900

    Start of the project

---
+++ Vivid_Strike!/Assets/Attack.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public GameObject Action;//동작
+
+    public float Action_frame = 0;
+
+    public float Start_frame = 0;
+
+    public Character Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Action_frame = gameObject.transform.parent.GetComponent<Character>().Timer;
+
+        //StartCoroutine("update");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {/*
+        if (Timer - Action_frame >= 9 * Time.deltaTime || Timer - Action_frame <= 11 * Time.deltaTime)
+        {
+            Preparation_Back();
+        }*/
+
+        Action_frame += Time.deltaTime;
+
+        Player.Preparation_Back();
+    }
+
+    public void Punch(float Frame_time)
+    {
+        //Destroy(action);
+
+        Start_frame = Frame_time;
+
+        Debug.Log("punch!");
+
+        Action = Instantiate(Resources.Load("Normal_Punch")) as GameObject;
+
+        Destroy(Action, 10 * Time.deltaTime);
+
+        //preparation = Instantiate(Resources.Load("Normal")) as GameObject;        
+
+        //Preparation_Back();
+    }
+    /*
+    public override void Preparation_Back()
+    {
+        base.Preparation_Back();
+
+        if(Start_frame != 0 && (Action_frame - Start_frame >= 9*Time.deltaTime && Action_frame - Start_frame <= 11*Time.deltaTime))
+        {
+            Debug.Log("Work?");
+
+            Preparation.SetActive(true);
+        }
+    }*/
+}
---
+++ Vivid_Strike!/Assets/Character.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public int Power;
+    public int Speed;
+    public int CounterPoint;
+    public int Stemina;
+    public int Reach;
+    public int Mana;
+        
+    public GameObject Preparation;//기본 동작
+
+    public Vector3 Position;//위치
+
+    public float Timer = 0;
+
+    public GameObject Player;
+
+    public Attack PunchAction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Preparation = Instantiate(Resources.Load("Normal")) as GameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer += Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            Preparation.SetActive(false);
+            //Destroy(preparation);
+
+            PunchAction.Punch(Timer);          
+        }        
+    }
+    /*
+    public virtual void Punch(float Frame_time)//float Frame_time
+    {
+
+    }
+    */
+    public void Preparation_Back()
+    {
+        //Debug.Log("Work?");
+
+        //Preparation.SetActive(true);        
+    }
+}
